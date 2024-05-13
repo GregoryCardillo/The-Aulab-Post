ab4031d4dcb6f92005bf917c4727078f94be0ec2
Gregory Cardillo <gregoryincmail@gmail.com>
2024-05-13 16:15:54 +0200

    completate categorie, creato modello article e funzioni di relazione, aggiunta  creazione/visualizzazione articoli e sezione categorie

--- resources/views/article/by-category.blade.php
+++ resources/views/article/by-category.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid p-5 bg-info text-center text-white">
         <div class="row justify-content-center">
             <h1 class="display-1">
-                The Aulab Post
+                Categoria: {{ $category->name }}
             </h1>
         </div>
     </div>
@@ -23,7 +23,7 @@
                     data="{{ $article->created_at->format('d/m/Y') }}"
                     user="{{ $article->user->name }}"
                     url="{{ route('article.show' , compact('article')) }}"
-                    urlCategory="{{ route('article.byCategory' , ['category' => $article->category->id]) }}"
+                    urlCategory="{{ route('article.byCategory' , ['category' => $article->category->id]) }}" 
                 />
             </div>
             @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PublicController;
+use App\Models\Article;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class , 'homepage'])->name('homepage');
+
+Route::get('/article/create' , [ArticleController::class , 'create'])->name('article.create');
+Route::post('/article/store' , [ArticleController::class , 'store'])->name('article.store');
+Route::get('/article/index' , [ArticleController::class , 'index'])->name('article.index');
+Route::get('/article/show/{article}' , [ArticleController::class , 'show'])->name('article.show');
+Route::get('/article/category/{category}' , [ArticleController::class, 'byCategory'])->name('article.byCategory');
