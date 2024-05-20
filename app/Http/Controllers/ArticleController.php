71f01b0914ec559b956673805d15841b806912b5
Gregory Cardillo <gregoryincmail@gmail.com>
2024-05-20 17:18:15 +0200

    Gestione delle categorie lato Admin

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,45 +17,45 @@ class ArticleController extends Controller
         $this->middleware('auth')->except('index' , 'show' , 'articleSearch');
     }
     /**
-     * Display a listing of the resource.
-     */
+    * Display a listing of the resource.
+    */
     public function index()
     {
         $articles = Article::where('is_accepted' , true)->orderBy('created_at' , 'desc')->get();
         return view('article.index' , compact('articles'));
     }
-
+    
     public function articleSearch(Request $request){
         $query = $request->input('query');
         $articles = Article::search($query)->where('is_accepted' , true)->orderby('created_at' , 'desc')->get();
-
+        
         return view('article.search-index' , compact('articles' , 'query'));
     }
-
+    
     public function byCategory(Category $category)
     {
         $articles = $category->articles()->where('is_accepted' , true)->orderby('created_at' , 'desc')->get();
         return view('article.by-category' , compact('category' , 'articles'));
     }
-
+    
     public function byUser(User $user)
     {
         $articles = $user->articles()->where('is_accepted' , true)->orderBy('created_at', 'desc')->get();
         return view('article.by-user', compact('user', 'articles'));
     }
-
-
+    
+    
     /**
-     * Show the form for creating a new resource.
-     */
+    * Show the form for creating a new resource.
+    */
     public function create()
     {
         return view('article.create');
     }
-
+    
     /**
-     * Store a newly created resource in storage.
-     */
+    * Store a newly created resource in storage.
+    */
     public function store(Request $request)
     {
         $request->validate([
@@ -66,60 +66,60 @@ class ArticleController extends Controller
             'category'=> 'required',
             'tags'=> 'required',
         ]);
-
+        
         $article = Article::create([
             'title'=> $request->title,
-            'subtitle'=>  $request->subtitle,
+            'subtitle'=> $request->subtitle,
             'body'=> $request->body,
             'image'=> $request->file('image')->store('public/images'),
             'category_id'=> $request->category,
             'user_id' => Auth::user()->id,
-            ]);
-
-            $tags = explode(',' , $request->tags);
-
-            foreach ($tags as $i => $tag) {
-                $tags[$i] = trim($tag);
-            }
-
-            foreach ($tags as $tag ) {
-                $newTag = Tag::updateOrCreate(
-                    ['name' => $tag],
-                    ['name' => strtolower($tag)],
-                );
-                $article->tags()->attach($newTag);
-            }
-
-            return redirect(route('homepage'))->with('message' , 'Articolo creato correttamente');
+        ]);
+        
+        $tags = explode(',' , $request->tags);
+        
+        foreach ($tags as $i => $tag) {
+            $tags[$i] = trim($tag);
+        }
+        
+        foreach ($tags as $tag ) {
+            $newTag = Tag::updateOrCreate(
+                ['name' => $tag],
+                ['name' => strtolower($tag)],
+            );
+            $article->tags()->attach($newTag);
+        }
+        
+        return redirect(route('homepage'))->with('message' , 'Articolo creato correttamente');
     }
-
+    
     /**
-     * Display the specified resource.
-     */
+    * Display the specified resource.
+    */
     public function show(Article $article)
     {
         return view('article.show', compact('article'));
     }
-
+    
     /**
-     * Show the form for editing the specified resource.
-     */
+    * Show the form for editing the specified resource.
+    */
     public function edit(Article $article)
     {
         //
     }
-
+    
     /**
-     * Update the specified resource in storage.
-     */
+    * Update the specified resource in storage.
+    */
     public function update(Request $request, Article $article)
     {
         //
     }
-
+    
     /**
-     * Remove the specified resource from storage.
-     */
+    * Remove the specified resource from storage.
+    */
     public function destroy(Article $article)
     {
         //
--- resources/views/components/metainfo-table.blade.php
+++ resources/views/components/metainfo-table.blade.php
@@ -3,50 +3,51 @@
         <tr>
             <th scope="col" >#</th>
             <th scope="col" >Nome Tag</th>
-            <th scope="col" >Articoli collegati</th>
+            <th scope="col" >Q.ta articoli collegati</th>
             <th scope="col" >Aggiorna</th>
             <th scope="col" >Cancella</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($metaInfos as $metaInfo)
-            <tr>
-                <th scope="row">{{ $metaInfo->id }}</th>
-                <td>{{ $metaInfo->name }}</td>
-                <td>{{ count($metaInfo->articles) }}</td>
-                @if ($metaType == 'tags')
-                    <td>
-                        <form action="{{ route('admin.editTag' , ['tag' => $metaInfo]) }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <input type="text" name="name" placeholder="Nuovo nome tag" class="form-control w-50 d-inline ">
-                            <button type="submit" class="btn btn-info text-white ">Aggiorna</button>
-                        </form>
-                    </td>
-                    <td>
-                        <form action="{{ route('admin.deleteTag' , ['tag' => $metaInfo]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger text-white ">Elimina</button>
-                        </form>
-                    </td>
-                    @else
-                    <form action="" method="">
-                        @csrf
-                        @method('put')
-                        <input type="text" name="name" placeholder="Nuovo nome categoria" class="form-control w-50 d-inline ">
-                        <button type="submit" class="btn btn-info text-white ">Aggiorna</button>
-                    </form>
-                </td>
-                <td>
-                    <form action="" method="">
-                        @csrf
-                        @method('delete')
-                        <button type="submit" class="btn btn-danger text-white ">Aggiorna</button>
-                    </form>
-
-                @endif
-            </tr>
-        @endforeach
-    </tbody>
+        <tr>
+            <th scope="row">{{ $metaInfo->id }}</th>
+            <td>{{ $metaInfo->name }}</td>
+            <td>{{ count($metaInfo->articles) }}</td>
+            @if ($metaType == 'tags')
+            <td>
+                <form action="{{ route('admin.editTag' , ['tag' => $metaInfo]) }}" method="POST">
+                    @csrf
+                    @method('put')
+                    <input type="text" name="name" placeholder="Nuovo nome tag" class="form-control w-50 d-inline ">
+                    <button class="btn btn-info text-white" type="submit">Aggiorna</button>
+                </form>
+            </td>
+            <td>
+                <form action="{{ route('admin.deleteTag' , ['tag' => $metaInfo]) }}" method="POST">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-danger text-white" type="submit">Elimina</button>
+                </form>
+            </td>
+            @else
+            <td>
+            <form action="{{ route('admin.editCategory' , ['category' => $metaInfo]) }}" method="POST">
+                @csrf
+                @method('put')
+                <input type="text" name="name" placeholder="Nuovo nome tag" class="form-control w-50 d-inline ">
+                <button class="btn btn-info text-white " type="submit" >Aggiorna</button>
+            </form>
+        </td>
+        <td>
+            <form action="{{ route('admin.deleteCategory' , ['category' => $metaInfo]) }}" method="POST">
+                @csrf
+                @method('delete')
+                <button class="btn btn-danger text-white" type="submit" >Elimina</button>
+            </form>
+        </td>
+        @endif
+    </tr>
+    @endforeach
+</tbody>
 </table>
